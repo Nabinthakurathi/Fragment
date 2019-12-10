145e59798b681c75903ebcb2e3c385ad90ab213e
lenovo <thakurathinabin@gmail.com>
2019-12-10 14:08:12 +0545

    some code added

--- app/src/main/java/com/nabin/fragment/MainActivity.java
+++ app/src/main/java/com/nabin/fragment/MainActivity.java
@@ -1,14 +1,43 @@
 package com.nabin.fragment;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener  {
+    private Button btnfragment;
+    private Boolean Status = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnfragment = findViewById(R.id.btnfragment);
+        btnfragment.setOnClickListener(this);
+    }
+
+    public void onClick(View v) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        if(Status){
+            firstFragment firstfragment = new firstFragment();
+            fragmentTransaction.replace(R.id.fragmentContainer,firstfragment);
+            fragmentTransaction.commit();
+            btnfragment.setText("Load second Fragment");
+            Status = false;
+        }
+        else{
+            SecondFragment secondFragment = new SecondFragment();
+            fragmentTransaction.replace(R.id.fragmentContainer,secondFragment);
+            fragmentTransaction.commit();
+            btnfragment.setText("Load first Fragment");
+            Status = true;
+        }
     }
 }
